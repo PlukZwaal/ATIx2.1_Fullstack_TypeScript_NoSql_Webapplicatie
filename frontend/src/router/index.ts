dac5731e38574098bba9dd1dccdba6a48c02dff1
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2025-09-30 15:53:24 +0200

    removed test

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/Home.vue'
-import TestView from '../views/Test.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,18 +6,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/Home.vue')
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('../views/About.vue')
     },
-    {
-      path: '/test',
-      name: 'test',
-      component: TestView
-    }
   ]
 })
 
