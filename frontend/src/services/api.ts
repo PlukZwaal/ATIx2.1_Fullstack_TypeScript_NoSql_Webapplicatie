ea0bcca8f8dd21fb8ffc86f2971ea8fb3eace87c
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2025-10-08 01:11:57 +0200

    fix

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -12,11 +12,11 @@ import type {
 } from '../types';
 import { STORAGE_KEYS } from '../constants';
 
-const baseURL = import.meta.env.VITE_API_URL2;
+const baseURL = import.meta.env.VITE_API_URL;
 
 if (!baseURL) {
   // Optioneel: gooi een foutmelding als de variabele niet is ingesteld
-  console.error("Omgevingsvariabele VITE_API_URL2 is niet ingesteld!");
+  console.error("Omgevingsvariabele VITE_API_URL is niet ingesteld!");
   // Of stel een fallback in als dat nodig is
   // throw new Error("API URL missing!");
 }
